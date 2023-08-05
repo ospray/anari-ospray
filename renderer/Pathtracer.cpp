496ecd5fd41278ffa927e9acdaa6df522d589706
Johannes Günther <johannes.guenther@intel.com>
2023-08-05 19:50:35 +0200

    Fixes

--- renderer/Pathtracer.cpp
+++ renderer/Pathtracer.cpp
@@ -15,14 +15,14 @@ void Pathtracer::commit()
   Renderer::commit();
 
   bool lightSamples = getParam<int>("lightSamples", -1);
-  int roulettePathDepth = getParam<int>("roulettePathDepth", 5);
+  int roulettePathLength = getParam<int>("roulettePathLength", 5);
   int maxScatteringEvents = getParam<int>("maxScatteringEvents", 20);
   float maxContribution = getParam<float>("maxContribution", 1e20f);
   bool backgroundRefraction = getParam<bool>("backgroundRefraction", false);
 
   auto r = osprayRenderer();
   ospSetInt(r, "lightSamples", lightSamples);
-  ospSetInt(r, "roulettePathDepth", roulettePathDepth);
+  ospSetInt(r, "roulettePathLength", roulettePathLength);
   ospSetInt(r, "maxScatteringEvents", maxScatteringEvents);
   ospSetFloat(r, "maxContribution", maxContribution);
   ospSetBool(r, "backgroundRefraction", backgroundRefraction);
