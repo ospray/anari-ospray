36e9de9f55616b6aa60df01682539046423aa450
Johannes Günther <johannes.guenther@intel.com>
2023-08-31 11:53:45 +0200

    Run all tests consecutively

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -5,20 +5,4 @@
 export LD_LIBRARY_PATH=./build/install/lib:./build/install/lib64:$LD_LIBRARY_PATH
 export ANARI_LIBRARY=ospray
 
-for s in \
-  instanced_cubes\
-  pbr_spheres\
-  random_cylinders\
-  random_spheres\
-  textured_cube\
-  triangle_attributes\
-; do
-  ./build/install/bin/anariRenderTests --scene test $s
-done
-
-for s in \
-  cornell_box\
-  gravity_spheres_volume\
-; do
-  ./build/install/bin/anariRenderTests --scene demo $s
-done
+./build/install/bin/anariRenderTests
