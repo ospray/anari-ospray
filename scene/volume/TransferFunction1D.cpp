74e24d082bb360f76c41715cd40d232a9473b3c0
Johannes Günther <johannes.guenther@intel.com>
2025-07-07 13:27:22 +0200

    TransferFunction1D: more flexible color and opacity types

--- scene/volume/TransferFunction1D.cpp
+++ scene/volume/TransferFunction1D.cpp
@@ -19,8 +19,24 @@ TransferFunction1DVolume::~TransferFunction1DVolume()
 void TransferFunction1DVolume::commitParameters()
 {
   m_field = getParamObject<SpatialField>("value");
+  if (!m_field) {
+    reportMessage(ANARI_SEVERITY_WARNING,
+        "no spatial field provided to transfer function");
+    return;
+  }
+
   m_colorData = getParamObject<Array1D>("color");
+  m_color4 = getParam<float4>(
+      "color", float4(getParam<float3>("color", float3(1.f)), 1.0f));
+  m_color4.w *= getParam<float>("opacity", 1.0f);
+
   m_opacityData = getParamObject<Array1D>("opacity");
+  if (m_opacityData && m_opacityData->elementType() != ANARI_FLOAT32) {
+    reportMessage(ANARI_SEVERITY_WARNING,
+        "opacity array must have elements of type ANARI_FLOAT32");
+    m_opacityData = nullptr;
+  }
+
   if (!getParam("valueRange", ANARI_FLOAT32_BOX1, &m_valueRange))
     m_valueRange = float2(0.f, 1.f);
   m_densityScale = getParam<float>("densityScale", 1.f);
@@ -28,30 +44,45 @@ void TransferFunction1DVolume::commitParameters()
 
 void TransferFunction1DVolume::finalize()
 {
-  if (!m_field) {
-    reportMessage(
-        ANARI_SEVERITY_WARNING, "no spatial field provided to transfer function");
+  if (!isValid())
     return;
-  }
 
-  if (!m_colorData) {
-    reportMessage(
-        ANARI_SEVERITY_WARNING, "no color data provided to transfer function");
-    return;
-  }
-
-  if (!m_opacityData) {
-    reportMessage(ANARI_SEVERITY_WARNING,
-        "no opacity data provided to transfer function");
-    return;
-  }
-
-  auto tf = m_osprayTF;
+  auto &tf = m_osprayTF;
   ospSetParam(tf, "value", OSP_BOX1F, &m_valueRange);
-  auto cd = m_colorData->osprayData();
+
+  if (m_colorData) {
+    m_color_opacity = convertToColorArray(*m_colorData);
+    for (auto &c : m_color_opacity)
+      c.w *= m_color4.w;
+  } else
+    m_color_opacity = std::vector<float4>(
+        m_opacityData ? m_opacityData->size() : 1, m_color4);
+
+  auto cd = ospNewSharedData1DStride(m_color_opacity.data(),
+      OSP_VEC3F,
+      m_color_opacity.size(),
+      sizeof(float4));
   ospSetParam(tf, "color", OSP_DATA, &cd);
-  auto od = m_opacityData->osprayData();
+  ospRelease(cd);
+
+  OSPData od;
+  const bool use =
+      m_opacityData && (m_opacityData->size() != m_color_opacity.size());
+  if (use)
+    od = m_opacityData->osprayData();
+  else {
+    if (m_opacityData)
+      for (size_t i = 0; i < m_color_opacity.size(); ++i)
+        m_color_opacity[i].w *= m_opacityData->beginAs<float>()[i];
+    od = ospNewSharedData1DStride(&m_color_opacity.data()->w,
+        OSP_FLOAT,
+        m_color_opacity.size(),
+        sizeof(float4));
+  }
+
   ospSetParam(tf, "opacity", OSP_DATA, &od);
+  if (!use)
+    ospRelease(od);
   ospCommit(tf);
 
   auto om = osprayModel();
@@ -64,7 +95,7 @@ void TransferFunction1DVolume::finalize()
 
 bool TransferFunction1DVolume::isValid() const
 {
-  return m_field && m_field->isValid() && m_colorData && m_opacityData;
+  return m_field && m_field->isValid();
 }
 
 } // namespace anari_ospray
